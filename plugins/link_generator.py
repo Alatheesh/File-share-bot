f057ccf3f3c558ed9630b700b6041be12311df79
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2023-07-23 13:21:12 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -42,7 +42,7 @@ async def batch(client: Client, message: Message):
 async def link_generator(client: Client, message: Message):
     while True:
         try:
-            channel_message = await client.ask(text="Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat=message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded timeout=60)
+            channel_message = await client.ask(text="Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat=message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         msg = await get_message_id(client, channel_message)
