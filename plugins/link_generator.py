0b887d9071746f3441320b759f50ba0cc377d331
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2023-07-23 10:16:42 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
-from utlis import get_shortlink
+from utils import get_shortlink
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
